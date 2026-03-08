7af39b19abdb47116074d381e0248c5e62ae4421
Ognyan Nankov <nankov.ognyan@gmail.com>
2026-03-08 23:54:37 +0200

    Add type data fetch service

--- src/services/pokemonService.js
+++ src/services/pokemonService.js
@@ -1,12 +1,13 @@
 import { getRandomPokemonId } from "../utils/random";
 import mapPokemondata from "../utils/pokemonUtils";
 
-const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
+const POKEMON_BASE_URL = "https://pokeapi.co/api/v2/pokemon";
+const TYPE_BASE_URL = "https://pokeapi.co/api/v2/type";
 
 export async function getRandomPokemon() {
     const id = getRandomPokemonId();
 
-    const response = await fetch(`${BASE_URL}/${id}`);
+    const response = await fetch(`${POKEMON_BASE_URL}/${id}`);
 
     if (!response.ok) {
         throw new Error("Failed to fetch Pokémon");
@@ -15,4 +16,15 @@ export async function getRandomPokemon() {
     const mappedPokemon = mapPokemondata(data)
 
     return mappedPokemon;
+}
+
+export async function getTypeByName(typeName) {
+    const response = await fetch(`${TYPE_BASE_URL}/${typeName}`);
+
+    if (!response.ok) {
+        throw new Error("Failed to fetch type data");
+    }
+
+    const data = await response.json()
+    return data;
 }
